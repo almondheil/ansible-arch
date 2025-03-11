4ead8a0124c833a383b0efdd583958ef95920f57
almond Heil <almond@almendra.dev>
2025-03-11 10:29:25 -0500

    Relocate inventory all into one dir

--- runme.sh
+++ runme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ANSIBLE_NOCOWS=1 ansible-playbook -i inventory.ini -K -l laptop setup_playbook.yaml $@
+ANSIBLE_NOCOWS=1 ansible-playbook -i inventory -K -l laptop setup_playbook.yaml $@
